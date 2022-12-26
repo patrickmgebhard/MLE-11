467d0d623d3a28a66fcd89cb103a0a4ae1e36256
Patrick Gebhard <patrickmgebhard@gmail.com>
2022-12-26 11:55:50 +0100

    working on week-03 assignment

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# pipeline = add the local pipeline here
 
 app = FastAPI()
+
+class TextToTranslate(BaseModel):
+    input_text: str
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
     
+@app.get("/ping")
+def ping():
+    return {"message": "ping"}
+
+@app.post("/echo")
+def echo(text_to_translate: TextToTranslate):
+    return {"message": text_to_translate.input_text}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
