682baca71329fa0c74c95355f1fdfa99536c330b
Patrick Gebhard <patrickmgebhard@gmail.com>
2022-12-28 21:27:10 +0100

    pushing .gitattributes to github

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -2,15 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
 
-# this is the code snippet from the t5-small page
-"""from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("t5-small")
-
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
-"""
-#pipeline = pipeline("")
-#en_fr_translator = pipeline("translation_en_to_fr")
+pipeline = pipeline(model="model/t5_small", task="translation_en_to_de")
 
 app = FastAPI()
 
@@ -28,6 +20,10 @@ def ping():
 @app.post("/echo")
 def echo(text_to_translate: TextToTranslate):
     return {"message": text_to_translate.input_text}
+
+@app.post("/translate")
+def translate(text):
+    return pipeline(text)
 
 if __name__ == "__main__":
     import uvicorn
